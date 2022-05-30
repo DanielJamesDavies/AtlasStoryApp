ac18a8079acaad46862544e19641d86a61322aa0
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2022-05-30 21:29:07 +0100

    Delete User

--- back-end/routes/UserRoute.js
+++ back-end/routes/UserRoute.js
@@ -11,6 +11,7 @@ const VerifyUser = require("../services/User/VerifyUser");
 const LogInUser = require("../services/User/LogInUser");
 const LogOutUser = require("../services/User/LogOutUser");
 const UpdateUser = require("../services/User/UpdateUser");
+const DeleteUser = require("../services/User/DeleteUser");
 
 router.get("/", GetUser);
 router.get("/:id", GetUserByID);
@@ -18,6 +19,7 @@ router.post("/", CookieConsentAuthentication, CreateNewUser);
 router.post("/verify", VerifyUser);
 router.post("/login", CookieConsentAuthentication, LogInUser);
 router.post("/logout", CookieConsentAuthentication, LogOutUser);
-router.patch("/", CookieConsentAuthentication, UpdateUser);
+router.patch("/", CookieConsentAuthentication, Authenticate, UpdateUser);
+router.delete("/", Authenticate, DeleteUser);
 
 module.exports = router;
---
+++ back-end/services/User/DeleteUser.js
@@ -0,0 +1,49 @@
+const jwt_decode = require("jwt-decode");
+
+const User = require("../../models/User");
+const UserVerification = require("../../models/UserVerification");
+const Image = require("../../models/Image");
+
+module.exports = async (req, res) => {
+	try {
+		var { user_id } = jwt_decode(req?.cookies?.AtlasStoryAppToken);
+	} catch (error) {
+		return res.status(200).send({ errors: [{ message: "Authentication Error" }] });
+	}
+
+	let user = await User.findById(user_id)
+		.exec()
+		.catch((error) => {
+			res.status(200).send({ error: error });
+		});
+	if (!user) return res.status(200).send({ errors: [{ message: "User Not Found" }] });
+
+	try {
+		const userDeleteResult = await User.deleteOne({ _id: user_id });
+		if (userDeleteResult?.deletedCount !== 1) return res.status(200).send({ errors: [{ message: "User Could Not Be Deleted" }] });
+	} catch (error) {
+		return res.status(200).send({ errors: [{ message: "User Could Not Be Deleted" }] });
+	}
+
+	if (user?.data?.profilePicture) {
+		try {
+			const profilePictureDeleteResult = await Image.deleteOne({ _id: user.data.profilePicture });
+			if (profilePictureDeleteResult?.deletedCount !== 1)
+				return res.status(200).send({ errors: [{ message: "Profile Picture Could Not Be Deleted" }] });
+		} catch (error) {
+			return res.status(200).send({ errors: [{ message: "Profile Picture Could Not Be Deleted" }] });
+		}
+	}
+
+	if (user?.verified) {
+		try {
+			const userVerificationDeleteResult = await UserVerification.deleteOne({ user_id });
+			if (userVerificationDeleteResult?.deletedCount !== 1)
+				return res.status(200).send({ errors: [{ message: "User Verification Could Not Be Deleted" }] });
+		} catch (error) {
+			return res.status(200).send({ errors: [{ message: "User Verification Could Not Be Deleted" }] });
+		}
+	}
+
+	return res.status(200).send({ message: "Success" });
+};
