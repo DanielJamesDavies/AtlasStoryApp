f397d9c4b715b038d54b0e7a8659d7f9e10d0565
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2022-07-02 13:54:28 +0100

    Character Ability Keep Index on Version Change

--- front-end/src/pages/Character/CharacterSubpages/Subpages/Abilities/AbilitiesLogic.jsx
+++ front-end/src/pages/Character/CharacterSubpages/Subpages/Abilities/AbilitiesLogic.jsx
@@ -15,23 +15,30 @@ import { CharacterContext } from "../../../CharacterContext";
 // Assets
 
 export const AbilitiesLogic = () => {
-	const { characterVersion, changeCharacterVersion } = useContext(CharacterContext);
+	const { character, characterVersion, changeCharacterVersion } = useContext(CharacterContext);
 	const [ability, setAbility] = useState(false);
 
 	useEffect(() => {
 		function getAbility() {
-			const oldAbility = JSON.parse(JSON.stringify(ability));
-			console.log(oldAbility);
-			// Get index of old ability
 			let newAbility = false;
+			let index = 0;
+
+			const oldAbility = JSON.parse(JSON.stringify(ability));
+			if (oldAbility !== false) {
+				const oldVersion = character?.data?.versions?.find((e) => e?._id === oldAbility?.character_version_id);
+				const oldIndex = oldVersion?.abilities?.findIndex((e) => e?._id === oldAbility?._id);
+				if (oldIndex !== -1) index = oldIndex;
+			}
+
 			if (characterVersion.abilities.length > 0) {
-				newAbility = JSON.parse(JSON.stringify(characterVersion.abilities[0]));
+				if (characterVersion.abilities.length - 1 < index) index = characterVersion.abilities.length - 1;
+				newAbility = JSON.parse(JSON.stringify(characterVersion.abilities[index]));
 				newAbility.character_version_id = characterVersion?._id;
 			}
 			return newAbility;
 		}
 		if (ability?.character_version_id !== characterVersion?._id) setAbility(getAbility());
-	}, [characterVersion, ability]);
+	}, [character, characterVersion, ability]);
 
 	function changeAbility(newAbility) {
 		setAbility(newAbility);
