8dc6edd8b8537e1c2be561b6fc926519baf0ffae
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2023-10-20 21:47:58 +0100

    Locations Path Bug Fix

--- front-end/src/pages/Locations/SpaceMap/Components/Path/Path.jsx
+++ front-end/src/pages/Locations/SpaceMap/Components/Path/Path.jsx
@@ -35,10 +35,9 @@ export const Path = ({ path, locations }) => {
 	useLayoutEffect(() => {
 		const [pos_from, pos_to] = getPointPositons();
 
-		async function createPath(pos_from, pos_to, iteration) {
+		async function createPath(pos_from, pos_to) {
 			if (!lineRef?.current || !cylinderGeometryRef?.current) return false;
-			if (JSON.stringify(currPositions.current) === JSON.stringify([pos_from, pos_to])) return false;
-			if (!isLoading.current) return false;
+			// if (JSON.stringify(currPositions.current) === JSON.stringify([pos_from, pos_to])) return false;
 
 			const newLength = Math.hypot(pos_from[0] - pos_to[0], pos_from[1] - pos_to[1], pos_from[2] - pos_to[2]);
 			setLength(newLength);
@@ -46,16 +45,10 @@ export const Path = ({ path, locations }) => {
 			lineRef.current.position.copy(new Vector3(...pos_from));
 			lineRef.current.lookAt(new Vector3(...pos_to));
 
-			cylinderGeometryRef.current.rotateX(Math.PI / 2);
-			cylinderGeometryRef.current.translate(0, 0, newLength / 2);
-
-			if (iteration === 1) {
-				isLoading.current = false;
-				currPositions.current = [pos_from, pos_to];
-			}
+			isLoading.current = false;
+			currPositions.current = [pos_from, pos_to];
 		}
-		createPath(pos_from, pos_to, 0);
-		setTimeout(() => createPath(pos_from, pos_to, 1), 1);
+		setTimeout(() => createPath(pos_from, pos_to), 1);
 	}, [path, locations, getPointPositons, setWidth, setLength, isLoading, currPositions]);
 
 	useEffect(() => {
@@ -64,8 +57,12 @@ export const Path = ({ path, locations }) => {
 
 	return (
 		<mesh ref={lineRef} position={getPointPositons()[0]}>
-			<cylinderGeometry ref={cylinderGeometryRef} attach='geometry' args={[width, width, length, 25]} />
-			<meshBasicMaterial attach='material' color={path?.colour === undefined ? "#444444" : path?.colour} />
+			<mesh rotation={[Math.PI / 2, 0, 0]}>
+				<mesh position={[0, length / 2, 0]}>
+					<cylinderGeometry ref={cylinderGeometryRef} attach='geometry' args={[width, width, length, 25]} />
+					<meshBasicMaterial attach='material' color={path?.colour === undefined ? "#444444" : path?.colour} />
+				</mesh>
+			</mesh>
 		</mesh>
 	);
 };
